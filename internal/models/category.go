81209f6c483fb17de9b15935fc1bd3d8131968e0
danik <dan1ar3110@gmail.com>
2026-04-02 23:19:10 +0500

    added description

--- internal/models/category.go
+++ internal/models/category.go
@@ -9,10 +9,10 @@ type Category struct {
 	ID            int64           `json:"id"`
 	Name          json.RawMessage `json:"name"`
 	Slug          string          `json:"slug"`
+	Description   json.RawMessage `json:"description"`
 	ParentID      *int64          `json:"parent_id,omitempty"`
 	Preview       *string         `json:"preview,omitempty"`
 	CreatedAt     time.Time       `json:"created_at"`
 	MediaID       *int64          `json:"-"`
 	MediaFileName *string         `json:"-"`
 }
-
