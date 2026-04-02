08cf503439f2ce7e1bc3f86da613ac16e37df857
danik <dan1ar3110@gmail.com>
2026-04-02 21:44:29 +0500

    category name struct

--- internal/models/category.go
+++ internal/models/category.go
@@ -1,10 +1,13 @@
 package models
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 type Category struct {
-	ID        int64     `json:"id"`
-	Name      string    `json:"name"`
+	ID        int64           `json:"id"`
+	Name      json.RawMessage `json:"name"`
 	Slug      string    `json:"slug"`
 	ParentID  *int64    `json:"parent_id,omitempty"`
 	CreatedAt time.Time `json:"created_at"`
