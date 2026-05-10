879af6a7b0dc9474a7a3551a1ce154e8b8aa69f1
danik <dan1ar3110@gmail.com>
2026-05-10 23:25:21 +0500

    feat: sentry to telegram

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -26,23 +27,29 @@ func New(ctx context.Context) *Logger {
 }
 
 func (l *Logger) Info(msg string, args ...any) {
-	log.Printf("[INFO] "+msg, args...)
-	l.sentry.Info().Emitf(msg, args...)
+	full := fmt.Sprintf(msg, args...)
+	log.Printf("[INFO] %s", full)
+	l.sentry.Info().Emit(full)
 }
 
 func (l *Logger) Warn(msg string, args ...any) {
-	log.Printf("[WARN] "+msg, args...)
-	l.sentry.Warn().Emitf(msg, args...)
+	full := fmt.Sprintf(msg, args...)
+	log.Printf("[WARN] %s", full)
+	l.sentry.Warn().Emit(full)
 }
 
 func (l *Logger) Error(msg string, args ...any) {
-	log.Printf("[ERROR] "+msg, args...)
-	l.sentry.Error().Emitf(msg, args...)
+	full := fmt.Sprintf(msg, args...)
+	log.Printf("[ERROR] %s", full)
+	l.sentry.Error().Emit(full)
+	sentry.CaptureMessage(full)
 }
 
 func (l *Logger) Fatal(msg string, args ...any) {
-	log.Printf("[FATAL] "+msg, args...)
-	l.sentry.Fatal().Emitf(msg, args...)
+	full := fmt.Sprintf(msg, args...)
+	log.Printf("[FATAL] %s", full)
+	l.sentry.Fatal().Emit(full)
+	sentry.CaptureMessage(full)
 	sentry.Flush(2 * time.Second)
 	os.Exit(1)
 }
